602d482ed12ab9db88bc4b725174ae437f92db62
Josh Lane <jplane@gmail.com>
2018-02-04 15:18:28 -0500

    fixing some bugs

--- DeviceEventHandler/EventPublisher.cs
+++ DeviceEventHandler/EventPublisher.cs
@@ -1,11 +1,17 @@
+
+using System;
 using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DeviceEventHandler
 {
@@ -24,9 +30,18 @@ namespace DeviceEventHandler
 
             http.DefaultRequestHeaders.Add("aeg-sas-key", sasKey);
 
-            foreach(var evt in events)
+            foreach (var evt in events)
             {
-                await http.PostAsync(topicUri, evt);
+                var wrapperEvt = new
+                {
+                    id = evt.msgid,
+                    eventType = "deviceInfo",
+                    subject = evt.deviceid,
+                    eventTime = evt.processedts,
+                    data = evt
+                };
+
+                var response = await http.PostAsync(topicUri, new[] { wrapperEvt }, new JsonMediaTypeFormatter());
 
                 log.Info("Published device event.");
             }
